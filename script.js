8534202121b0be36f3fa417c39bc239a165fc865
Alex Straton <alexandrastraton1@gmail.com>
2024-06-05 13:18:40 +0100

    page is made responsive and interactive with onclick events for all buttons

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("hi");
+const github = document.getElementById("github_btn");
+const frontendMentor = document.getElementById("fm_btn");
+const linkedin = document.getElementById("linkedin_btn");
+const twitter = document.getElementById("twitter_btn");
+const instagram = document.getElementById("ig_btn");
+
+github.addEventListener("click", () => {
+  location.href = "https://github.com/AlexStraton";
+});
+
+frontendMentor.addEventListener("click", () => {
+  location.href = "https://www.frontendmentor.io/profile/AlexStraton";
+});
+
+linkedin.addEventListener("click", () => {
+  location.href = "https://www.linkedin.com/in/alexandra-straton-2747a019b/";
+});
+
+twitter.addEventListener("click", () => {
+  location.href = "https://x.com/Alexand61303491";
+});
+
+instagram.addEventListener("click", () => {
+  location.href = "https://www.instagram.com/worldtraveler.tck/";
+});
